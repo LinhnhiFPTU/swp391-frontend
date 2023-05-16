223e6962ffb845a5cf341240fa8aa88af2c61587
DevDD <duylvd2002@gmail.com>
2023-05-16 16:37:58 +0700

    16:37  16/05

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import classNames from "classnames/bind";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
 import Header from "~/layouts/components/Header/Header";
 import Footer from "~/layouts/components/Footer";
-
-import banner from "~/assets/images/banner.jpg";
-import birds from "~/assets/images/birds.png";
-import birdfood from "~/assets/images/bird-foods.png";
-import birdaccessories from "~/assets/images/bird-accessories.png";
+import Banner from "~/layouts/components/Banner/";
+// import birds from "~/assets/images/birds.png";
+// import birdfood from "~/assets/images/bird-foods.png";
+// import birdaccessories from "~/assets/images/bird-accessories.png";
 // import bird from "~/assets/images/bird.png";
 
 import styles from "./Home.module.scss";
@@ -23,16 +21,7 @@ function Home() {
       <div className={cx("container")}>
         <div className={cx("content")}>
           {/* -----------------BANNER----------------- */}
-          <div className={cx("banner")}>
-            <img src={banner} alt="banner"></img>
-            <div className={cx("banner-content")}>
-              <h1 className={cx("content-text")}>Keep Your Pet Safe</h1>
-              <Link to="/shop" className={cx("content-button")}>
-                SHOP NOW
-              </Link>
-            </div>
-          </div>
-
+          <Banner/>
           {/* -----------------CATEGORIES----------------- */}
           {/* <div className={cx("categories")}>
             <div className={cx("bird-cage_cat")}>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import Login from "~/pages/Login";
 import Signup from '~/pages/Signup'
 import Forget from '~/pages/ForgetPassword'
 
+
 const publicRoutes = [
   { path: '/', component: Home},
   { path: '/login', component: Login},
   { path: '/signup', component: Signup},
   { path: '/reset', component: Forget},
+
 ];
 
 const privateRoutes = [];
