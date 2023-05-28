57bb0f7f0aea3d9acbd5ad51a77bef29f91c79e9
DevDD <duylvd2002@gmail.com>
2023-05-28 16:47:15 +0700

    16:47  28/05

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ import Address from "~/pages/Profile/Address"
 import FlashSale from '~/pages/FlashSale'
 import ProductSale from "~/layouts/components/ProductSale";
 import Shop from "~/pages/Shop";
+import ProductSearch from "~/pages/ProductSearch";
 
 
 const publicRoutes = [
@@ -24,8 +25,9 @@ const publicRoutes = [
   { path: '/user/account/password', component: Password},
   { path: '/user/account/address', component: Address},
   { path: '/flash_sale', component: FlashSale},
-  { path: '/product_sale', component: ProductSale},
   { path: '/shop', component: Shop},
+  { path: '/product_sale', component: ProductSale},
+  { path: '/product_search', component: ProductSearch},
 ];
 
 const privateRoutes = [];
