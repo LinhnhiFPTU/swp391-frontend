42890244252fff2d3e58c592af26f9c0b2c7817d
devduyled2203 <duylvd2002@gmail.com>
2023-07-27 22:21:16 +0700

    27/07 22:20 Update Shipper

--- src/pages/Shipper/Shipper.js
+++ src/pages/Shipper/Shipper.js
@@ -1,15 +1,21 @@
 import classNames from "classnames/bind";
 
+import OrderPopup from "./OrderPopup";
 import styles from "./Shipper.module.scss";
 
 import HeaderShipper from "./HeaderShipper";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
 function Shipper() {
-  
+  const [openPopup, setOpenPopup] = useState(false);
+  const handleOpenPopup = () => {
+    setOpenPopup(true);
+  };
   return (
     <>
+      {openPopup && <OrderPopup closePopup={setOpenPopup}/>}
       <HeaderShipper />
       <div className={cx("shipper_container")}>
         <div className={cx("shipper_content")}>
@@ -33,6 +39,48 @@ function Shipper() {
             </form>
           </div>
           <div className={cx("order_result")}>
+            <div className={cx("result")} onClick={handleOpenPopup}>
+              <div className={cx("order-code")}>#1234456</div>
+              <div className={cx("order-detail")}>
+                <div className={cx("product-detail")}>
+                  <img
+                    src="https://m.media-amazon.com/images/I/81cR4gm3+aL._AC_SL1500_.jpg"
+                    alt="product-img"
+                    className={cx("product-img")}
+                  />
+                  <div className={cx("product-info")}>
+                    <div className={cx("name")}>
+                      Prevue Pet Products Travel Carrier for Birds, Black
+                    </div>
+                    <div className={cx("quantity")}>x2</div>
+                    <div className={cx("price")}>2000$</div>
+                  </div>
+                </div>
+                <div className={cx("user-detail")}>
+                  <div className={cx("user-info")}>
+                    <div className={cx("username")}>Le Vu Dinh Duy</div>
+                    <div className={cx("phone")}>0123456789</div>
+                    <div className={cx("address")}>
+                      S305 Vinhomes Grand Park, 9 District
+                    </div>
+                  </div>
+                </div>
+                <div className={cx("action")}>
+                  <div className={cx("action-btn")}>
+                    <div className={cx("approve")}>
+                      <button className={cx("approve-btn")}>
+                        Successful delivery
+                      </button>
+                    </div>
+                    <div className={cx("reject")}>
+                      <button className={cx("reject-btn")}>
+                        Unsuccessful delivery
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className={cx("result")}>
               <div className={cx("order-code")}>#1234456</div>
               <div className={cx("order-detail")}>
@@ -67,11 +115,147 @@ function Shipper() {
                       </button>
                     </div>
                     <div className={cx("reject")}>
-                      <button className={cx("reject-btn")}>Unsuccessful delivery</button>
+                      <button className={cx("reject-btn")}>
+                        Unsuccessful delivery
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+            <div className={cx("result")}>
+              <div className={cx("order-code")}>#1234456</div>
+              <div className={cx("order-detail")}>
+                <div className={cx("product-detail")}>
+                  <img
+                    src="https://m.media-amazon.com/images/I/81cR4gm3+aL._AC_SL1500_.jpg"
+                    alt="product-img"
+                    className={cx("product-img")}
+                  />
+                  <div className={cx("product-info")}>
+                    <div className={cx("name")}>
+                      Prevue Pet Products Travel Carrier for Birds, Black
+                    </div>
+                    <div className={cx("quantity")}>x2</div>
+                    <div className={cx("price")}>2000$</div>
+                  </div>
+                </div>
+                <div className={cx("user-detail")}>
+                  <div className={cx("user-info")}>
+                    <div className={cx("username")}>Le Vu Dinh Duy</div>
+                    <div className={cx("phone")}>0123456789</div>
+                    <div className={cx("address")}>
+                      S305 Vinhomes Grand Park, 9 District
+                    </div>
+                  </div>
+                </div>
+                <div className={cx("action")}>
+                  <div className={cx("action-btn")}>
+                    <div className={cx("approve")}>
+                      <button className={cx("approve-btn")}>
+                        Successful delivery
+                      </button>
+                    </div>
+                    <div className={cx("reject")}>
+                      <button className={cx("reject-btn")}>
+                        Unsuccessful delivery
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={cx("result")}>
+              <div className={cx("order-code")}>#1234456</div>
+              <div className={cx("order-detail")}>
+                <div className={cx("product-detail")}>
+                  <img
+                    src="https://m.media-amazon.com/images/I/81cR4gm3+aL._AC_SL1500_.jpg"
+                    alt="product-img"
+                    className={cx("product-img")}
+                  />
+                  <div className={cx("product-info")}>
+                    <div className={cx("name")}>
+                      Prevue Pet Products Travel Carrier for Birds, Black
+                    </div>
+                    <div className={cx("quantity")}>x2</div>
+                    <div className={cx("price")}>2000$</div>
+                  </div>
+                </div>
+                <div className={cx("user-detail")}>
+                  <div className={cx("user-info")}>
+                    <div className={cx("username")}>Le Vu Dinh Duy</div>
+                    <div className={cx("phone")}>0123456789</div>
+                    <div className={cx("address")}>
+                      S305 Vinhomes Grand Park, 9 District
+                    </div>
+                  </div>
+                </div>
+                <div className={cx("action")}>
+                  <div className={cx("action-btn")}>
+                    <div className={cx("approve")}>
+                      <button className={cx("approve-btn")}>
+                        Successful delivery
+                      </button>
+                    </div>
+                    <div className={cx("reject")}>
+                      <button className={cx("reject-btn")}>
+                        Unsuccessful delivery
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={cx("result")}>
+              <div className={cx("order-code")}>#1234456</div>
+              <div className={cx("order-detail")}>
+                <div className={cx("product-detail")}>
+                  <img
+                    src="https://m.media-amazon.com/images/I/81cR4gm3+aL._AC_SL1500_.jpg"
+                    alt="product-img"
+                    className={cx("product-img")}
+                  />
+                  <div className={cx("product-info")}>
+                    <div className={cx("name")}>
+                      Prevue Pet Products Travel Carrier for Birds, Black
+                    </div>
+                    <div className={cx("quantity")}>x2</div>
+                    <div className={cx("price")}>2000$</div>
+                  </div>
+                </div>
+                <div className={cx("user-detail")}>
+                  <div className={cx("user-info")}>
+                    <div className={cx("username")}>Le Vu Dinh Duy</div>
+                    <div className={cx("phone")}>0123456789</div>
+                    <div className={cx("address")}>
+                      S305 Vinhomes Grand Park, 9 District
+                    </div>
+                  </div>
+                </div>
+                <div className={cx("action")}>
+                  <div className={cx("action-btn")}>
+                    <div className={cx("approve")}>
+                      <button className={cx("approve-btn")}>
+                        Successful delivery
+                      </button>
+                    </div>
+                    <div className={cx("reject")}>
+                      <button className={cx("reject-btn")}>
+                        Unsuccessful delivery
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={cx("prev-next")}>
+              <button className={cx("icon-left")}>
+                <i className={cx("fa-light fa-angle-left")}></i>
+              </button>
+              <button className={cx("icon-right")}>
+                <i className={cx("fa-light fa-angle-right")}></i>
+              </button>
             </div>
           </div>
         </div>
